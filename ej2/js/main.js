0836da058108248e72dd1df670eb264c305c4cae
Clara Brígido <clarabrigido@gmail.com>
2019-08-06 09:15:31 +0200

    add eje 2

--- ej2/js/main.js
+++ ej2/js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let counter = 0;
+let temp;
+const time = document.querySelector('.time');
+
+
+const incrementAndShowCounter = () => {
+    counter++;
+    time.innerHTML += '🍈';
+    if (counter === 12) {
+        clearInterval(temp);
+        time.innerHTML = '¡Feliz año Nuevo!';
+    }
+};
+
+
+temp = setInterval(incrementAndShowCounter, 1000);
