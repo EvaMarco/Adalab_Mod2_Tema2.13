f3fc2869294423f3ccdaa6ba8e5d3558102c0e6e
Clara Brígido <clarabrigido@gmail.com>
2019-08-06 09:56:31 +0200

    add eje 4

--- ej4/js/main.js
+++ ej4/js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let counter = 0;
+const time = document.querySelector('.time');
+let stop;
+
+function showText() {
+    counter++;
+    console.log(counter);
+    if (counter === 15) {
+        time.innerHTML = 'Su sesión ha expirado';
+        time.classList.toggle('hidden');
+        clearInterval(stop);
+
+    }
+};
+stop = setInterval(showText, 1000);
